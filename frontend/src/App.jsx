1921eb20e763499a618796a31f6ca6c55fa7828a
jasminesayyad29 <jasminesayyad6603@gmail.com>
2025-03-14 15:33:50 +0530

    home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import Navbar from './components/Navbar/Navbar'
 import Login from './components/Login/Login';
 import Register from './components/Login/Register';
+import Home from './components/Home/Home'
+import { Home } from 'lucide-react';
 
 function App() {
   return (
@@ -11,7 +13,7 @@ function App() {
       <div>
         <Navbar />
         <Routes>
-          
+          <Route path="/" element={<Home/>}></Route>
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
 
