a0e5b6f6b9dcebd1e9bc56ea0db8408c9b2f7248
Aadarsh Nandedkar <aadarshnandedkar@gmail.com>
2025-05-03 00:55:35 +0530

    AV

--- backend/app.py
+++ backend/app.py
@@ -321,9 +321,11 @@ def recommend():
         logging.exception("🔥 /recommend error")
         return jsonify({"error": str(e)}), 500
 
+# if __name__ == '__main__':                                                                                      ### CHANGED
+#     app.run(host='0.0.0.0', port=5000)  
 
 # if __name__ == '__main__':                                                                                      ### CHANGED
-#     app.run(host='10.40.4.225', port=5000, ssl_context=('cert.pem', 'key.pem'))                                 ### CHANGED
+#     app.run(host='192.168.174.55', port=5000, ssl_context=('cert.pem', 'key.pem'))                                 ### CHANGED
 
 if __name__ == '__main__':
     app.run(debug=True)
